333280427a42ae4c7ab5317c681443f4001b38e8
Dendi Suhubdy <gpuhashcracker@protonmail.com>
2021-05-17 16:55:50 +0700

    feat: vectors

--- containers/src/main.cpp
+++ containers/src/main.cpp
@@ -10,10 +10,44 @@ auto foo() -> void {
     // Capacity
     // Size
     std::vector<int> value{1, 3, 4};
-    value.reserve(10);
+    value.reserve(5);
 
-    std::cout << value.size() << "\n";
-    std::cout << value.capacity() << "\n";
+    std::cout << "Size is " << value.size() << "\n";
+    std::cout << "Capacity is " << value.capacity() << "\n";
+
+    std::cout << "Elements of value are, by copy:" << "\n";
+
+    for (auto e: value) {
+        std::cout << e << "\n";
+    }
+
+    std::cout << "Elements of value are, by reference:" << "\n";
+
+    // auto& e == auto & e == auto &e
+
+    for (auto &e: value) {
+        std::cout << e << "\n";
+    }
+
+    std::cout << "With iterators \n";
+    std::vector<int>::iterator it = value.begin();
+    for (; it != value.end(); ++it) {
+        std::cout << *it << "\n";
+    }
+
+    value.push_back(10);
+    value.push_back(10);
+    value.push_back(10);
+    value.push_back(10);
+    value.push_back(10);
+    value.front();
+    value.back();
+    
+    // So erase always takes the iterator as a parameter
+    value.erase( value.begin() + 1);
+    
+    std::cout << "Size is " << value.size() << "\n";
+    std::cout << "Capacity is " << value.capacity() << "\n";
 }
 
 auto main() -> int {
